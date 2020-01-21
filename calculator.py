e6f8a3f090699cc90e0f4a7d32ef367742800eeb
Kshitiz Singh <ksingh23@asu.edu>
2020-01-20 21:29:51 -0700

    Integral visually shown via matplotlib library, region highlighted

--- calculator.py
+++ calculator.py
@@ -1,17 +1,18 @@
-import parser
-import numpy as np
 from math import cos
 import Equation as eq
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Polygon
 
 
 def main():
     formula = "cos(x)"
     func = eq.Expression("cos(x)", ["x"])
-    leftBound = 0
-    rightBound = np.pi/2
+    left_bound = 0
+    right_bound = np.pi/2
     numRect = 100
-    rectWidth = (rightBound - leftBound)/numRect
+    rectWidth = (right_bound - left_bound)/numRect
     change = True
     area = 0
     while change:
@@ -23,13 +24,21 @@ def main():
             start += rectWidth
 
         # print("Area: " + str(area))
-        if abs(area - oldArea) <= 0.00001:
+        if abs(area - oldArea) <= 0.001:
             change = False
         else:
             change = True
         numRect *= 2
-        rectWidth = (rightBound - leftBound) / numRect
-    print(area)
-
+        rectWidth = (right_bound - left_bound) / numRect
+    figure, axis = plt.subplots()
+    x = np.linspace(-np.pi, np.pi, 201)
+    axis.plot(x, func(x), 'r')
+    axis.set_ylim(bottom=0)
+    bounds = np.linspace(0, np.pi/2, 201)
+    integral_region = func(bounds)
+    verts = [(left_bound, 0), *zip(bounds, integral_region), (right_bound, 0)]
+    area_under_curve = Polygon(verts, facecolor='0.9', edgecolor='0.2', linewidth=0)
+    axis.add_patch(area_under_curve)
+    plt.show()
 
 main()
