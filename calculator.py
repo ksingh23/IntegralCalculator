33ec45dde79c07d36ddee84422cdd006a60140f2
Kshitiz Singh <ksingh23@asu.edu>
2020-02-03 12:41:17 -0700

    Simpson's rule implementation with proper integrals complete, need to work on it for improper ones e.g. ln(sin(x))

--- calculator.py
+++ calculator.py
@@ -22,23 +22,36 @@ def integral_with_riemann_sum(func, left_bound, right_bound):
         old_area = area
         area = 0
         for i in range(num_rect):
-            area += (func(start) * rect_width)
+            length = sp.N(func.subs(x, start))
+            area += (length * rect_width)
             start += rect_width
-
-        # print("Area: " + str(area))
         if abs(area - old_area) <= 0.001:
             change = False
         else:
             change = True
         num_rect *= 2
         rect_width = (right_bound - left_bound) / num_rect
-    return area
+    return sp.N(area)
 
 
-def integral_with_simpsons_rule(func, left_bound, right_bound, deriv):
-    error = 0.001
-    midpt = (right_bound - left_bound)/2
-    return ((right_bound - left_bound)/6) * (func(left_bound) + 4 * func(midpt) + func(right_bound))
+def integral_with_simpsons_rule(func, left_bound, right_bound, num_steps):
+    step_size = float((right_bound - left_bound)/num_steps)
+    def_integral = func.subs(x, (left_bound + sys.float_info.epsilon))
+    # Problem is that ln(sin(x)) at 0 is undefined, how do we solve this?
+    # Instead of inputting zero, I inputted 0 + epsilon to solve
+    i = left_bound + step_size
+    itr = 0
+    while i < (right_bound - step_size):
+        if itr % 2 == 0:
+            def_integral += (4 * func.subs(x, i))
+        else:
+            def_integral += (2 * func.subs(x, i))
+        itr += 1
+        i += step_size
+    # returns zoo = complex infinity
+    def_integral += (func.subs(x, i))
+    def_integral *= (step_size/3)
+    return sp.N(def_integral)
 
 
 def get_step_size(function, left_bound, right_bound, error):
@@ -46,28 +59,44 @@ def get_step_size(function, left_bound, right_bound, error):
     second_deriv = sp.diff(function, x, 2)
     third_deriv = sp.diff(second_deriv, x, 1)
     zeros = sp.solve(third_deriv, x)
-    zeros += [left_bound, right_bound]
-    m = -sys.maxsize - 1
+    lb = second_deriv.subs(x, left_bound)
+    rb = second_deriv.subs(x, right_bound)
+    if math.isinf(abs(lb)):
+        if math.isinf(abs(rb)):
+            m = -sys.maxsize - 1
+        else:
+            m = abs(rb)
+    else:
+        if math.isinf(abs(rb)):
+            m = abs(lb)
+        else:
+            m = max(abs(lb), abs(rb))
     for z in zeros:
         if z >= left_bound and z <= right_bound:
             value = abs(second_deriv.subs(x, z))
             if value > m:
                 m = value
-    return math.ceil(math.sqrt((m * (right_bound - left_bound)**3)/(12 * error)))
+    num_steps = math.ceil(math.sqrt((m * (right_bound - left_bound)**3)/(12 * error)))
+    if num_steps % 2 != 0:
+        num_steps += 1
+    return num_steps
 
 
 if __name__ == '__main__':
-    formula = "ln(sin(x))"
+    formula = "ln(x)"
     x = sp.symbols('x')
     sp.init_printing(use_unicode=True)
     func = eq.Expression(formula, ["x"])
     function = parse_expr(formula)
-    lam_f = lambdify(x, function, 'numpy')
-    left_bound = sp.pi/6
-    right_bound = sp.pi / 2
+    left_bound = 0
+    right_bound = sp.pi/2
     start = time.time()
-    print(get_step_size(function, left_bound, right_bound, 0.0001))
+    num_steps = get_step_size(function, left_bound, right_bound, 0.0001)
+    print(integral_with_simpsons_rule(function, left_bound, right_bound, num_steps))
     print(time.time() - start)
+    # start = time.time()
+    # print(integral_with_riemann_sum(function, left_bound, right_bound))
+    # print(time.time() - start)
     '''
     
     integral_with_riemann_sum(func, left_bound, right_bound)
